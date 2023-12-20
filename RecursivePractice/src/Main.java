0b866fe10325b48fd8a0781d8c9fc6816cb35c04
maergur <153439629+maergur@users.noreply.github.com>
2023-12-21 00:36:21 +0300

    Fonksiyon ismi güncellendi.

--- RecursivePractice/src/Main.java
+++ RecursivePractice/src/Main.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class Main {
-    public static int multiply(int a, int b){
+    public static int powerUp(int a, int b){
 
         if (b == 0) {
             return 1;
         } else {
             // Recursive case
-            return a * multiply(a, b - 1);
+            return a * powerUp(a, b - 1);
         }
 
 
@@ -22,7 +22,7 @@ public class Main {
         System.out.print("Lütfen üs giriniz: ");
         power = inp.nextInt();
 
-        System.out.print("İşlem sonucu: " + multiply(base,power));
+        System.out.print("İşlem sonucu: " + powerUp(base,power));
 
     }
 }
