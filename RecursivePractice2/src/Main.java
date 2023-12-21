a745efb6a906a87761b8bbd482b5cde798ac6aad
maergur <153439629+maergur@users.noreply.github.com>
2023-12-21 11:29:37 +0300

    Recursive Practice 2 programı tamamlandı.

---
+++ RecursivePractice2/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Lütfen bir sayı giriniz: ");
+        int num = scanner.nextInt();
+
+        // 0 ve 1 asal sayı değildir.
+
+        if (num < 2) {
+            System.out.println(num + " Asal bir sayı değildir.");
+        }
+
+        // 0 ve 1 dışındaki sayıları  test ediyoruz.
+
+        else {
+            if (isPrime(num, 2)) {
+                System.out.println(num + " Asal bir sayıdır.");
+            } else {
+                System.out.println(num + " Asal bir sayı değildir.");
+            }
+        }
+
+        scanner.close();
+    }
+
+    // Recursive asal sayı kontrolü yaparken 2'ye bölme sebebimiz recursive methodu limitlemek
+    public static boolean isPrime(int a, int divider) {
+        if (divider > a / 2) {
+            return true;
+        }
+
+        // Mod alarak asal olup olmadığına bakıyoruz.
+
+        else {
+            if (a % divider == 0) {
+                return false;
+            } else {
+                return isPrime(a, divider + 1);
+            }
+        }
+    }
+}
