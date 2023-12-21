32d573b575fa2e477bb2ab7c4334a4c21838106c
maergur <153439629+maergur@users.noreply.github.com>
2023-12-21 23:11:25 +0300

    Recursive Practice 3 programı tamamlandı.

--- RecursivePractice3/src/Main.java
+++ RecursivePractice3/src/Main.java
@@ -1,30 +1,38 @@
 import java.util.Scanner;
 
 public class Main {
+    /*
+    Kural : Girilen sayı 0 veya negatif olduğu yere kadar girilen
+    sayıdan 5 rakamını çıkarın. Her çıkarma işlemi sırasında ekrana
+    son değeri yazdırın. Sayı negatif veya 0 olduktan sonra tekrar
+    5 ekleyin. Yine her ekleme işleminde sayının son değerini ekrana yazdırın.
 
+     */
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Lütfen bir sayı giriniz: ");
-        int num = inp.nextInt();
+        int n = scanner.nextInt();
 
-        recursiveIslem(sayi);
+        recursive(n);
     }
 
-    // Recursive olarak işlem yapma metodu
-    public static void recursiveIslem(int sayi) {
-        if (sayi <= 0) {
-            System.out.println("Son Değer: " + sayi);
-            // Eğer sayı 0 veya negatifse işlemi sonlandır
+    public static void recursive(int n) {
+        if (n <= 0) {
+            System.out.println(n + " ");
             return;
-        } else {
-            // Her çıkarma işlemi sırasında sayıyı ekrana yazdır
-            System.out.println("Çıkarma Sonucu: " + sayi);
-            recursiveIslem(sayi - 5);
-
-            // Her ekleme işlemi sırasında sayıyı ekrana yazdır
-            System.out.println("Toplama Sonucu: " + sayi);
-            recursiveIslem(sayi + 5);
         }
+
+        /* Bunu yazmam saatlerimi aldı aslında ama çözüm yoyo mantığı gibi.
+
+        Method kendi içinde loop'a girerek girilen sayıdan 0 veya negatife gidiyor.
+
+        Ancak o noktada return'e düştüğü için bu sefer kendini yoyo gibi başa sarıyor.
+
+        Umarım açıklayacı olmuştur :)
+         */
+
+        System.out.println(n +  " ");
+        recursive(n - 5);
+        System.out.println(n + "");
     }
 }
